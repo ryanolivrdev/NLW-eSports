ce8da3eaa792af050d017bd474edb4f4413529b0
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-09-16 01:48:29 -0300

    feat(mobile) add routes and api conect

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,8 @@ import {
   Inter_900Black,
 } from "@expo-google-fonts/inter";
 
-import { Home } from "./src/screens/Home";
+
+import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
 import { Background } from "./src/components/Background";
 
@@ -22,7 +23,7 @@ export default function App() {
   return (
     <Background>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,52 @@
+import { Text } from 'react-native';
+import { TouchableOpacity, View } from "react-native";
+import { GameController } from "phosphor-react-native";
+
+import { DuoInfo } from "../DuoInfo";
+
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+
+export interface DuoCardProps {
+  hourEnd: string;
+  hourStart: string;
+  id: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: String[];
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+  onConnected: () => void;
+}
+
+export function DuoCard({ data, onConnected }: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo label="Nome" value={data.name} />
+
+      <DuoInfo label="Tempo de jogo" value={`${data.yearsPlaying} anos`} />
+
+      <DuoInfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+      />
+
+      <DuoInfo
+        label="Chamada de áudio?"
+        value={data.useVoiceChannel ? "Sim" : "Não"}
+        colorValue={
+          data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT
+        }
+      />
+
+      <TouchableOpacity style={styles.button} onPress={onConnected}>
+        <GameController color={THEME.COLORS.TEXT} size={20} />
+
+        <Text style={styles.buttonTitle}>Conectar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
